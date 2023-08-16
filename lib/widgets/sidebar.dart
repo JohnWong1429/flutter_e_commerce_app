c60dec2481673b25c2261c254eb7156ddaccaad8
John <john-cpwong@vgt-hk.net>
2023-08-16 11:59:06 +0800

    Add logout item to side bar

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_e_commerce_app/pages/login_page.dart';
 
 class SideBar extends StatelessWidget {
   const SideBar({super.key});
@@ -6,24 +7,47 @@ class SideBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: const [
-          DrawerHeader(
-            child: Text('User Info'),
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              shrinkWrap: true,
+              children: const [
+                DrawerHeader(
+                  child: Text('User Info'),
+                ),
+                ListTile(
+                  title: Text('Home'),
+                ),
+                ListTile(
+                  title: Text('Products'),
+                ),
+                ListTile(
+                  title: Text('Cart'),
+                ),
+                ListTile(
+                  title: Text('About Us'),
+                ),
+              ],
+            ),
           ),
           ListTile(
-            title: Text('Home Page'),
+            title: const Text(
+              'Log out',
+              style: TextStyle(
+                color: Color.fromARGB(255, 190, 13, 0),
+              ),
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed(LoginPage.routeName);
+            },
+            leading: const Icon(
+              Icons.logout,
+            ),
           ),
-          ListTile(
-            title: Text('Products'),
-          ),
-          ListTile(
-            title: Text('Cart'),
-          ),
-          ListTile(
-            title: Text('About Us'),
-          ),
+          const SizedBox(
+            height: 30,
+          )
         ],
       ),
     );
